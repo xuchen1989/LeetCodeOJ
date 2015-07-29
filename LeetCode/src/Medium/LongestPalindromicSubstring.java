e0fcf2a4bf567e92e47d0bf4ce0e8b707540cb3c
xuchen1989 <chenxu.fdu@gmail.com>
2015-07-29 13:48:29 +0800

    final version for longest palin

--- LeetCode/src/Medium/LongestPalindromicSubstring.java
+++ LeetCode/src/Medium/LongestPalindromicSubstring.java
@@ -7,5 +7,33 @@ package Medium;
  *and there exists one unique longest palindromic substring.
  */
 public class LongestPalindromicSubstring {
+	public String longestPalindrome(String s) {
+		if (s==null || "".equals(s.trim()) || s.length()==1)
+			return s;
+		int length = s.length();
+		int max = 0;
+		String maxStirng = "";
+		for(int i=0;i<length*2-1;i++){
+			int left = i/2;
+			int right =i/2;
+			if(i%2==1){
+				right++;
+			}
+			String palindromicString = getPalin(left,right,s);
+			if(palindromicString.length()>max){
+				max = palindromicString.length();
+				maxStirng = palindromicString;
+			}
+		}
+		return maxStirng;
+		
+    }
 
+	private String getPalin(int left, int right, String s) {
+		while(left>=0 && right<=s.length()-1 && s.charAt(left)==s.charAt(right)){
+			left--;
+			right ++;
+		}
+		return s.substring(left+1, right);
+	}
 }
